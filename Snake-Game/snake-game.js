f36b676bc3e3bd34f1323de3e414ddb683c76fbd
NikolaiK420 <nikolaikumanov006@gmail.com>
2024-09-26 19:28:48 +0300

    made snake and reloading food

--- Snake-Game/snake-game.js
+++ Snake-Game/snake-game.js
@@ -2,8 +2,16 @@
 const blockSize = 25;
 const rows = 30;
 const cols = 30;
-var board;
-var context;
+let board;
+let context;
+
+//snake head
+let snakeHeadX = blockSize * 3;
+let snakeHeadY = blockSize * 3;
+
+//food
+let foodX;
+let foodY;
 
 window.onload = function() {
     board = document.getElementById("board");
@@ -12,9 +20,23 @@ window.onload = function() {
     context = board.getContext("2d");
 
     update();
+    placeFood();
 }
 
 function update() {
     context.fillStyle = "black";
     context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle = "lime";
+    context.fillRect(snakeHeadX, snakeHeadY, blockSize, blockSize);
+
+}
+
+function placeFood(){
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
+
+    context.fillStyle = "red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+
 }
